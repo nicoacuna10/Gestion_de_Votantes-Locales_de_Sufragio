0235549f5cac3dcddba4286181cf2665cfe834da
Nicolás Acuña <80644036+nicoacuna10@users.noreply.github.com>
2022-03-31 17:20:49 -0300

    Update Votante.java

--- GestionDeVotantes/src/main/java/Votante.java
+++ GestionDeVotantes/src/main/java/Votante.java
@@ -10,24 +10,17 @@ public class Votante {
     private String nombreCompleto; // Nombre del votante.
     private String rut; // Rut del votante.
     private String comuna; // Comuna de domiclio.
-    private String direccion; // Dirección del domicilio.
-    
-    // Coordenadas de la ubicación del domicilio.
-    private int coordenadaX;
-    private int coordenadaY;
-    
+    private String direccion; // Dirección del domicilio.    
     private int estadoElectoral; // Si el valor es 1, el votante está habilitado para votar.
     private String localAsignado; // Nombre del local donde le toca votar.
     private int numeroDeMesa; // Número de mesa donde le toca votar.
     
     // Constructor de la clase
-    public Votante(String nombreCompleto, String rut, String comuna, String direccion, int coordenadaX, int coordenadaY, int estadoElectoral, String localAsignado, int numeroDeMesa) {
+    public Votante(String nombreCompleto, String rut, String comuna, String direccion, int estadoElectoral, String localAsignado, int numeroDeMesa) {
         this.nombreCompleto = nombreCompleto;
         this.rut = rut;
         this.comuna = comuna;
         this.direccion = direccion;
-        this.coordenadaX = coordenadaX;
-        this.coordenadaY = coordenadaY;
         this.estadoElectoral = estadoElectoral;
         this.localAsignado = localAsignado;
         this.numeroDeMesa = numeroDeMesa;
@@ -44,12 +37,6 @@ public class Votante {
     }
     public String getDireccion() {
         return direccion;
-    }
-    public int getCoordenadaX() {
-        return coordenadaX;
-    }
-    public int getCoordenadaY() {
-        return coordenadaY;
     }
     public int getEstadoElectoral() {
         return estadoElectoral;
@@ -72,12 +59,6 @@ public class Votante {
     }
     public void setDireccion(String direccion) {
         this.direccion = direccion;
-    }
-    public void setCoordenadaX(int coordenadaX) {
-        this.coordenadaX = coordenadaX;
-    }
-    public void setCoordenadaY(int coordenadaY) {
-        this.coordenadaY = coordenadaY;
     }
     public void setEstadoElectoral(int estadoElectoral) {
         this.estadoElectoral = estadoElectoral;
