5fb69cebef6c7bed1355fbc0c061987d3ac2febd
Nicolás Acuña <80644036+nicoacuna10@users.noreply.github.com>
2022-06-29 17:40:49 -0400

    Add files via upload

---
+++ src/Controlador/GestionDeVotantes.java
@@ -0,0 +1,36 @@
+package Controlador;
+
+
+import Modelo.CSV;
+import Modelo.Funcionalidad;
+import Modelo.Region;
+import Vista.VentanaMenu;
+import java.io.*;
+
+/**
+ * GestionDeVotantes es la clase principal
+ * @author jpail
+ * @author Nicolás Acuña
+ */
+public class GestionDeVotantes {
+    
+
+    /**
+     * Función principal de la clase
+     * @param args args
+     * @throws IOException Excepcion Input/Output
+     */
+    public static void main(String[] args)throws IOException{
+        Region valparaiso = new Region();
+        Funcionalidad f = new Funcionalidad();
+        CSV administradorCSV = new CSV();
+        
+        f.funcionInicial(valparaiso);
+        administradorCSV.importarCSV("datosImportar.csv", valparaiso);
+        
+        VentanaMenu menu = new VentanaMenu(valparaiso, f, administradorCSV);
+        menu.setVisible(true);
+        
+    }
+    
+}
